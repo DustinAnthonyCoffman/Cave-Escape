a80f87a0f8e937601ea27dfe8b2d0cdd3e9cfe14
Dustin <dustin.anthony.coffman@gmail.com>
2019-11-01 17:02:29 -0500

    health bar display

--- js/main.js
+++ js/main.js
@@ -7,10 +7,16 @@ let safetyButton = document.querySelectorAll('.safe');
 
 /*----- app's state (variables) -----*/
 
+let health = document.querySelector('.health').textContent;
+let healthInt = parseInt(health);
+let healthDisplay = document.querySelector('.health');
+let healthTopDisplay = document.querySelector('.topHealth');
+let healthLeftDisplay = document.querySelector('.leftHealth');
+let healthRightDisplay = document.querySelector('.rightHealth');
+let healthBottomDisplay = document.querySelector('.bottomHealth');
 
 /*----- cached element references -----*/
 let satchel = [];
-let playerHealth = 100;
 let torch = 'torch';
 let raft = 'raft';
 let battleAxe = 'Battle Axe';
@@ -26,11 +32,15 @@ function reduceHealth () {
     currentRoom = 'danger';
     let deathTimer2 = setInterval(function() {
         if (currentRoom === 'danger') {
-            playerHealth -= 5;
-            console.log('you are dying', playerHealth);
+            healthInt -= 5;
+            healthDisplay.innerHTML = healthInt.toString();
+            healthTopDisplay.innerHTML = healthInt.toString();
+            healthBottomDisplay.innerHTML = healthInt.toString();
+            healthLeftDisplay.innerHTML = healthInt.toString();
+            healthRightDisplay.innerHTML = healthInt.toString();
         }
         else {
-            clearInterval(deathTimer2);
+            clearInterval(deathTimer2);   
         }  
     }, 2000);    
 };
@@ -38,7 +48,7 @@ function reduceHealth () {
 
 function reachSafety() {
     currentRoom = 'safe';
-    alert('you are safe!! ' + playerHealth);
+    alert('you are safe!! ');
 };
 
 
